bc2f5515a435b2d09eaf2a49cef95649ecf11428
Unknown <s3530160@student.rmit.edu.au>
2018-04-07 20:35:16 +1000

    Resync Info

--- SEF/src/hr_system/Task.java
+++ SEF/src/hr_system/Task.java
@@ -1,6 +1,6 @@
 package hr_system;
 
-public class Task
+public class Task extends TaskArea
 {
 	public String taskId, taskTitle, location, day, taskType, startDate, endDate,
 	taskQual, notes;
@@ -16,15 +16,83 @@ public class Task
 		this.taskTitle = taskTitle;
 	}
 	
-	public void getTask()
+	
+	
+	
+	//SET METHODS
+	public void setTask(String taskId, String taskTitle, String location, String day,
+						String taskType, String startDate, String endDate, String taskQual,
+						String notes, double taskLengthHrs, int time)
 	{
-		
+		setTaskId(taskId);
+		setTaskTitle(taskTitle);
+		setLocation(location);
+		setDay(day);
+		setTaskType(taskType);
+		setStartDate(startDate);
+		setEndDate(endDate);
+		setTaskQual(taskQual);
+		setNotes(notes);
+		setTaskLengthHrs(taskLengthHrs);
+		setTime(time);
+	}
+	public void setTaskId(String taskId)
+	{
+		this.taskId = taskId;
 	}
 	
-	public void setTask()
+	public void setTaskTitle(String taskTitle)
 	{
-		
+		this.taskTitle = taskTitle;
 	}
+	
+	public void setLocation(String location)
+	{
+		this.location = location;
+	}
+	
+	public void setDay(String day)
+	{
+		this.day = day;
+	}
+	
+	public void setTaskType(String taskType)
+	{
+		this.taskType = taskType;
+	}
+	
+	public void setStartDate(String startDate)
+	{
+		this.startDate = startDate;
+	}
+	
+	public void setEndDate(String endDate)
+	{
+		this.endDate = endDate;
+	}
+	
+	public void setTaskQual(String taskQual)
+	{
+		this.taskQual = taskQual;
+	}
+	
+	public void setNotes(String notes)
+	{
+		this.notes = notes;
+	}
+	
+	public void setTaskLengthHrs(double taskLengthHrs)
+	{
+		this.taskLengthHrs = taskLengthHrs;
+	}
+	
+	public void setTime(int time)
+	{
+		this.time = time;
+	}	
+	//END OF SET METHODS
+	
+	
 	
 	public boolean createTask()		//parameters needs to be coded
 	{
@@ -32,9 +100,16 @@ public class Task
 		return false;
 	}
 	
-	public boolean editTask()
+	public boolean editTask(int taskNum, String taskId, String taskTitle, String location, String day,
+							String taskType, String startDate, String endDate, String taskQual,
+							String notes, double taskLengthHrs, int time)
 	{
-		
+		if (tasks[taskNum] != null)
+		{
+			setTask(taskId,taskTitle,location,day,taskType,startDate,endDate,taskQual,notes,taskLengthHrs,
+					time);
+			return true;
+		}
 		return false;
 	}
 	
