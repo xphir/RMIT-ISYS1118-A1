fbe67d3ce944fd76293f3de5a6db6443289b5ed6
Unknown <dahuzi66@gmail.com>
2018-04-07 20:14:01 +1000

    Deleting duplicate

--- SEF_project/src/hr_system/CasualStaff.java
+++
@@ -1,6 +0,0 @@
-package hr_system;
-
-public class CasualStaff
-{
-	public String csID, firstName, lastName, title, email, qualification;
-}
--- SEF_project/src/hr_system/HRAgent.java
+++
@@ -1,7 +0,0 @@
-package hr_system;
-
-public class HRAgent
-{
-	public String agentID, firstName, lastName, agentType, accessArea;
-	
-}
