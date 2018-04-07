98dab75578b0c8a22ce2c64772c932b4807739c8
Unknown <s3530160@student.rmit.edu.au>
2018-04-07 20:40:43 +1000

    Adding Sreeja's Code
    Note need to fix code outside methods - Elliot

--- SEF/src/hr_system/Task.java
+++ SEF/src/hr_system/Task.java
@@ -92,11 +92,68 @@ public class Task extends TaskArea
 	}	
 	//END OF SET METHODS
 	
-	
+	//Sreeja to fix START
+	Scanner scanner = new Scanner(System.in);
+	tasks[] t = new tasks[20];	
+	int t_count = 0;
+	//Sreeja to fix END
 	
 	public boolean createTask()		//parameters needs to be coded
 	{
+		System.out.println("Enter the folllowing details to add a task");  
 		
+		System.out.println(" Enter the ID of the task:");
+		String t_ID = scanner.nextLine();
+		
+		System.out.println(" Enter the length of the task:");
+		Double t_len = scanner.nextDouble();
+		
+		System.out.println(" Enter the title of the task:");
+		String t_tit = scanner.nextLine();
+
+		System.out.println(" Enter the location of the task:");
+		String t_loc = scanner.nextLine();
+		
+		System.out.println(" Enter the day of the task:");
+		String t_day = scanner.nextLine();
+		
+		System.out.println(" Enter the time of the task:");
+		String t_time = scanner.nextLine();
+		
+		System.out.println(" Enter the type of the task:");
+		String t_type = scanner.nextLine();
+		
+		System.out.println(" Enter the start date of the task:");
+		String t_start= scanner.nextLine();
+		
+		System.out.println(" Enter the end date of the task:");
+		String t_end= scanner.nextLine();
+		
+		System.out.println(" Enter the course code of the task:");
+		String t_courseCode= scanner.nextLine();
+		
+		System.out.println(" Enter the qualification of the task:");
+		String t_qual= scanner.nextLine();
+		
+		System.out.println(" Enter the qualification of the task:");
+		String t_assign= scanner.nextLine();
+		
+        t[t_count] = new tasks();
+
+        t[t_count].setTaskID(t_ID);
+        t[t_count].setTask_lengthHrs(t_len);
+        t[t_count].setTask_title(t_tit);
+        t[t_count].setLocation(t_loc);
+        t[t_count].setTask_type(t_type);
+        t[t_count].setStart_date(t_start);
+        t[t_count].setEnd_date(t_end);
+        t[t_count].setCourse_code(t_courseCode);
+        t[t_count].setTask_qual(t_qual);
+        t[t_count].setAssigned_staff(t_assign);
+        
+        
+        if (t_count < 21)
+        	t_count++;
 		return false;
 	}
 	
@@ -115,7 +172,26 @@ public class Task extends TaskArea
 	
 	public boolean deleteTask()
 	{
-		
+		int temp1 = 0;     //the task id to be deleted 
+        
+        System.out.println(" Enter the ID of the task to delete:");
+		String taskID_1 = scanner.nextLine();
+       	
+        for (tasks ta : t)
+        {
+            if (ta.getTaskID() == taskID_1)
+            {
+                break;
+            }
+            temp1++;
+        }
+
+        while (temp1 < t_count)
+        {
+            t[temp1] = t[temp1 + 1];
+            temp1++;
+        }
+        t[temp1] = null;
 		return false;
 	}
 	
