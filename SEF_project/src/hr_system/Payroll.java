2e036281cd79d61decf5d82e2b16cb65e378af97
dahuzi66 <38095972+dahuzi66@users.noreply.github.com>
2018-04-07 15:16:48 +1000

    Add files via upload

---
+++ SEF_project/src/hr_system/Payroll.java
@@ -0,0 +1,12 @@
+package hr_system;
+
+public class Payroll
+{
+	public String staffName, staffTitle, staffMail;
+	public double wage, workingHours, totalPay;
+	
+	public void generatePayroll()
+	{
+		
+	}
+}
